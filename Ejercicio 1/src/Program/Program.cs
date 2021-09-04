3e9f4178902e77a1e93282bb72466fb322e40762
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-09-04 15:20:09 -0300

    Fix Program.cs code

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -7,20 +7,15 @@ namespace SRP
     {
         static void Main(string[] args)
         {
-            /*
             Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034");
             Libro libro2 = new Libro("Pro C#","Troelsen","001-035");
-            libro1.AlmacenarLibro("A","7");
-            libro2.AlmacenarLibro("B","3");
-            */
-            Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034");
-            Libro libro2 = new Libro("Pro C#","Troelsen","001-035");
-            Sector secA = new Sector("A");
-            Sector secB = new Sector("B");
-            secA.AñadirEstante("7");
-            secB.AñadirEstante("3");
-            secA.AñadirLibro("7", libro1);
-            secB.AñadirLibro("3", libro2);
+            Biblioteca biblioteca = new Biblioteca();
+            biblioteca.AñadirSector("A");
+            biblioteca.AñadirSector("B");
+            biblioteca.AñadirEstante("A", "7");
+            biblioteca.AñadirEstante("B", "3");
+            biblioteca.AñadirLibro("A", "7", libro1);
+            biblioteca.AñadirLibro("B", "3", libro2);
         }
     }
 }
