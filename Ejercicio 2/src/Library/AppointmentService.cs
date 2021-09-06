6916967495b0a7c3acbd825cb9771e934708dd1d
Santiago De Olivera <santiago.deolivera@correo.ucu.edu.uy>
2021-09-06 09:00:14 -0300

    Reorganize responsibilities and fix comments

--- Ejercicio 2/src/Library/AppointmentService.cs
+++ Ejercicio 2/src/Library/AppointmentService.cs
@@ -8,55 +8,29 @@ namespace Library
     public class AppointmentService
     {
 
-        // Es más conveniente que una instancia de AppointmentService almacene la información, y tenga un método para devolver el texto
-        public readonly string Name;
-        public readonly byte Age;
-        public readonly string Id;
-        public readonly string PhoneNumber;
+        // Es más conveniente que una instancia de AppointmentService almacene la
+        // información, y tenga un método para devolver el texto.
+
+        // La información sobre el paciente de la cita pasa a una clase `Patient`,
+        // dado que no está intrínsecamente asociada a la cita.
+
+        // Lo mismo con la información sobre el doctor.
+
+        public readonly Patient PatientData;
         public readonly DateTime Date;
         public readonly string Place;
-        public readonly string DoctorName;
-        public readonly string DoctorSpecialty;
+        public readonly Doctor DoctorData;
 
-        private static List<string> ids = new List<string>();
-
-        public AppointmentService(string name, byte age, string id, string phoneNumber, DateTime date, string appointmentPlace, string doctorName, string doctorSpecialty)
+        public AppointmentService(Patient patientData, DateTime date, string appointmentPlace, Doctor doctorData)
         {
-            // Name validation (avoid null and empty cases)
-            if(name == null || name.Length == 0) throw new ArgumentException("A name is required");
-
-            // ID validation (avoid null and empty cases)
-            if(id == null || id.Length == 0) throw new ArgumentException("Id is required");
-            // ID validation (ids must be different from one another)
-            if(ids.Contains(id)) throw new ArgumentException($"There's already an appointment with the id \"{id}\"");
-
-            // Phone number validation (avoid null and empty cases)
-            if(phoneNumber == null || phoneNumber.Length == 0) throw new ArgumentException("Phone number is required");
-            // Phone number validation (it has to have 10 digits)
-            if(phoneNumber.Where(c => Char.IsDigit(c)).Count() != 10)
-                throw new ArgumentException($"A phone number must have 10 digits");
+            if(patientData == null) throw new ArgumentException("Patient data is required");
+            if(string.IsNullOrEmpty(appointmentPlace)) throw new ArgumentException("Appointment place is required");
+            if(doctorData == null) throw new ArgumentException("Doctor data is required");
             
-            // Date validation (avoid null case)
-            if(date == null) throw new ArgumentException("Date is required");
-            
-            // Appointment place validation (avoid null and empty cases)
-            if(appointmentPlace == null || appointmentPlace.Length == 0) throw new ArgumentException("Appointment place is required");
-            
-            // Doctor name validation (avoid null case)
-            if(doctorName == null || doctorName.Length == 0) throw new ArgumentException("Doctor name is required");
-
-            // Doctor specialty validation (avoid null and empty cases)
-            if(doctorSpecialty == null || doctorSpecialty.Length == 0) throw new ArgumentException("Doctor specialty is required");
-            
-            this.Name = name;
-            this.Age = age;
-            this.Id = id;
-            ids.Add(id);
-            this.PhoneNumber = phoneNumber;
+            this.PatientData = patientData;
             this.Date = date;
             this.Place = appointmentPlace;
-            this.DoctorName = doctorName;
-            this.DoctorSpecialty = doctorSpecialty;
+            this.DoctorData = doctorData;
         }
 
         public string ToText()
@@ -64,34 +38,22 @@ namespace Library
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
             Boolean isValid = true;
 
-            if (string.IsNullOrEmpty(this.Name))
+            if(this.PatientData == null)
             {
-                stringBuilder.Append("Unable to schedule appointment, Name is required\n");
-                isValid = false;
-            }
-
-            if (string.IsNullOrEmpty(this.Id))
-            {
-                stringBuilder.Append("Unable to schedule appointment, id is required\n");
-                isValid = false;
-            }
-
-            if (string.IsNullOrEmpty(this.PhoneNumber))
-            {
-                stringBuilder.Append("Unable to schedule appointment, Phone number is required\n");
+                stringBuilder.Append("Unable to schedule appointment, Patient data is required\n");
                 isValid = false;
             }
 
             if (string.IsNullOrEmpty(this.Place))
             {
-                stringBuilder.Append("Unable to schedule appointment, Appoinment place is required\n");
+                stringBuilder.Append("Unable to schedule appointment, Appointment place is required\n");
                 isValid = false;
             }
 
             
-            if (string.IsNullOrEmpty(this.DoctorName))
+            if (this.DoctorData == null)
             {
-                stringBuilder.Append("Unable to schedule appointment, Doctor name is required\n");
+                stringBuilder.Append("Unable to schedule appointment, Doctor data is required\n");
                 isValid = false;
             }
 
--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,16 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            AppointmentService appointment1 = new AppointmentService("Steven Jhonson", 30, "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand", "Medic");
+            Doctor doctor1 = new Doctor("Armand", "Medic");
+            Doctor doctor2 = new Doctor("Susan", "Medic");
+
+            Patient patient1 = new Patient("Steven Jhonson", 30, "986782342", "5555-555-555");
+            Patient patient2 = new Patient("Ralf Manson", 20, "", "5555-555-555");
+            
+            AppointmentService appointment1 = new AppointmentService(patient1, DateTime.Now, "Wall Street", doctor1);
             Console.WriteLine(appointment1.ToText());
 
-            AppointmentService appointment2 = new AppointmentService("Ralf Manson", 20, "", "5555-555-555", DateTime.Now, "Queen Street", "", "Medic");
+            AppointmentService appointment2 = new AppointmentService(patient2, DateTime.Now, "Queen Street", doctor2);
             Console.WriteLine(appointment2.ToText());
         }
     }
